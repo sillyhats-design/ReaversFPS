1ce3da7dcee5789b2be0f1a3aab0e88b9104f99f
Kyle Young <KEYoung@student.fullsail.edu>
2022-11-02 21:38:25 -0400

    Placeholder for Wave and Ammo, Reticle Change and Script added

---
+++ ReaversFPS/Assets/Scripts/Player/Reticle.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticle : MonoBehaviour
+{
+    // Placeholder Code for changing reticle when enemy is hit
+}
