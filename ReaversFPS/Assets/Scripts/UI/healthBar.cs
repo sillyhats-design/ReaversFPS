7dc88f1d71732b675fde040647d9e629274efd90
Kyle Young <KEYoung@student.fullsail.edu>
2022-11-06 21:29:14 -0500

    Health Bar now Working

--- ReaversFPS/Assets/Scripts/UI/healthBar.cs
+++ ReaversFPS/Assets/Scripts/UI/healthBar.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 
 public class healthBar : MonoBehaviour
 {
+    public GameObject player;
     public playerController playerHealth;
     public Image fillImage;
     private Slider slider;
 
+
     // Start is called before the first frame update
     void Awake()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         slider = GetComponent<Slider>();
+        playerHealth = player.GetComponent<playerController>();
     }
 
     // Update is called once per frame
