fb6e67f8557b222e46c22487c072c11bf85e7203
mickeymiguelmsu <majimenez@student.fullsail.edu>
2022-11-06 23:47:46 -0500

    gunStats components added, UI added to my scene

--- ReaversFPS/Assets/Scripts/Game Manager/gunStats.cs
+++ ReaversFPS/Assets/Scripts/Game Manager/gunStats.cs
@@ -7,4 +7,11 @@ using UnityEngine;
 public class gunStats : ScriptableObject
 {
     public int ammoCount;
+    public int damage;
+    public int fireRate;
+    public float range;
+    public AudioClip audioClip;
+    public GameObject model;
+    public GameObject effectFlash;
+    public GameObject effectHit;
 }
